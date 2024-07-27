1540ef8989fd50098b01441d7b9470dfd23c33a5
Hoang-261 <huyhoang.tv.hy@gmail.com>
2024-07-27 23:09:06 +0700

    connect to mangodb atlas

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
 async function connect() {
   try {
-    await mongoose.connect("mongodb://localhost:27017/lacdau_dev");
+    await mongoose.connect(
+      "mongodb+srv://lacdau:lacdauPass@cluster0.kn5t0t0.mongodb.net/lacdau_server?retryWrites=true&w=majority&appName=Cluster0"
+    );
     console.log("connect successfully !");
   } catch (error) {
     console.log("connect failure!");
